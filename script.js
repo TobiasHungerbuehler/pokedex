c89d3566d82b3de49551852eaf9f386d63203391
enterskynet <76483987+TobiasHungerbuehler@users.noreply.github.com>
2023-04-24 10:06:29 +0200

    class List bug solved

--- script.js
+++ script.js
@@ -32,78 +32,57 @@ function createIconContainer(pokemonData, i) {
     document.getElementById('stage').innerHTML += /*html*/ `
         <div class="icon-container" id="icon-container${i}" onclick="showFullCard(${i+1})"></div>
     `;
-    createCard(pokemonData, i, 'icon-container'+i, 'card'+i)
+    createCard(pokemonData, i)
 }
 
 
-function createCard(pokemonData, i, parent, cardId) {
-    let pokemonName = pokemonData['forms'][0]['name'];
-    document.getElementById(parent).innerHTML += /*html*/ `
-            <div class="card" id='${cardId}'>
-                <img class="icon-ball" id="icon-ball${cardId.substring(4)}" src="./img/ball2.png" alt="">
-                <div class="card-top">
-                    <img class="icon-pokedex-logo" src="./img/pokedex.png" alt="">
-                    <h1 id="name">${pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1)}</h1>
-                    <img class="icon-pokemon-img" src="${pokemonData['sprites']['other']['dream_world']['front_default']}" alt="">
-                    <div  class="number-container">
-                        <h3># ${pokemonData['id']}</h3>
-                    </div>
-                </div>
+function createCard(pokemonData, i) {
+    document.getElementById('icon-container'+i).innerHTML += /*html*/ `
+            <div class="card" id='card${i}'>
+                <img class="icon-ball" id="icon-ball${i}" src="./img/ball2.png" alt="">
             </div>
-        `;
-    setBgColor(pokemonData, cardId);
-    console.log(cardId)
+            `;
+    showCardTop(pokemonData, i)
+    setBgColor(pokemonData, 'card'+i);
+}
+
+
+function showCardTop(pokemonData, i) {
+    console.log('showCardTop= ', i)
+    let pokemonName = pokemonData['forms'][0]['name'];
+    let pokemonNameEdit = pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1);
+    let pokemonImg = pokemonData['sprites']['other']['dream_world']['front_default'];
+    let pokemonId = pokemonData['id'];
+    createCardTopHTML(i, pokemonNameEdit, pokemonImg, pokemonId)
+}
+
+
+function createCardTopHTML(i, pokemonNameEdit, pokemonImg, pokemonId) {
+    console.log('showCardTop= ', i)
+    document.getElementById('card'+i).innerHTML = /*html*/ `
+        <div class="card-top">
+            <img class="icon-pokedex-logo" src="./img/pokedex.png" alt="">
+            <h1 id="name">${pokemonNameEdit}</h1>
+            <img class="icon-pokemon-img" src="${pokemonImg}" alt="">
+            <div  class="number-container">
+                <h3># ${pokemonId}</h3>
+            </div>
+        </div>
+    `;
 }
 
 
 function setBgColor(pokemonData, parent) {
-    const colorClass = pokemonData['types'][0]['type']['name'];
+    console.log(parent);
+    let colorClass = pokemonData['types'][0]['type']['name'];
     for (let i = 0; i < validClasses.length; i++) {
         if (colorClass === validClasses[i]) {
+            console.log(colorClass);
         document.getElementById(parent).classList.add(colorClass);
+
         break;}
         }   
     }
-
-
-// function setBgColor2(pokemonData,  parent) {
-//     let colorClass = pokemonData['types'][0]['type']['name'];
-
-//     if (colorClass == 'grass') {
-//         document.getElementById(parent).classList.add(colorClass);
-//     }
-//     if (colorClass == 'fire') {
-//         document.getElementById(parent).classList.add(colorClass);
-//     }
-//     if (colorClass == 'water') {
-//         document.getElementById(parent).classList.add(colorClass);
-//     }
-//     if (colorClass == 'bug') {
-//         document.getElementById(parent).classList.add(colorClass);
-//     }
-//     if (colorClass == 'normal') {
-//         document.getElementById(parent).classList.add(colorClass);
-//     }
-//     if (colorClass == 'poison') {
-//         document.getElementById(parent).classList.add(colorClass);
-//     }
-//     if (colorClass == 'electric') {
-//         document.getElementById(parent).classList.add(colorClass);
-//     }
-//     if (colorClass == 'ground') {
-//         document.getElementById(parent).classList.add(colorClass);
-//     }
-//     if (colorClass == 'fairy') {
-//         document.getElementById(parent).classList.add(colorClass);
-//     }
-//     if (colorClass == 'fighting') {
-//         document.getElementById(parent).classList.add(colorClass);
-//     }
-//     if (colorClass == 'psychic') {
-//         document.getElementById(parent).classList.add(colorClass);
-//     }  
-// }
-
 
 //Show Full Card ////////////////////////////////////
 
@@ -116,54 +95,69 @@ async function showFullCard(pokemonId) {
 
 function showOverlay(pokemonData, pokemonId) {
     document.getElementById('overlay').classList.remove('d-none') 
-    //createCardHeader(pokemonData, pokemonId)
-
     setBgColor(pokemonData, 'card-1');
+    showCardTop(pokemonData, -2);
+    showDetails(pokemonId);
 }
 
 
-function createCardHeader(pokemonData, pokemonId) {
-        createCard(pokemonData, pokemonId, 'overlay', 'card'+-1)
-        document.getElementById('card'+-1).classList.add('full-card');
-        document.getElementById('icon-ball'+-1).classList.add('ball-rotation');
-        
-        showNavigation();
-        showDetails(selectetPokemon);
-
-}
-
-
-function showNavigation(){
-    document.getElementById('card-1').innerHTML += /*html*/ `
-        <div class="card-nav">
-            <button onclick="newFullCard(selectetPokemon -1)" ><</button>
-            <button onclick="closeCard()" >Schliessen</button>
-            <button onclick="newFullCard(selectetPokemon +1)" >></button>
-        </div>
-    `;
-}
-
-
-async function showDetails(selectetPokemon) {
-    let url = `https://pokeapi.co/api/v2/pokemon-species/${selectetPokemon}`;
+async function showDetails(pokemonId) {
+    let url = `https://pokeapi.co/api/v2/pokemon-species/${pokemonId}`;
     let response = await fetch(url);
     let responseAsJson = await response.json();
     //return responseAsJson;
+
+    document.getElementById('description').innerHTML = responseAsJson['flavor_text_entries'][25]['flavor_text'];
     console.log(responseAsJson['flavor_text_entries'][25]['flavor_text']); // Beschreibungstext !
 
 }
 
+// function createCardHeader(pokemonData, pokemonId) {
+//         createCard(pokemonData, pokemonId, 'overlay', 'card'+-1)
+//         document.getElementById('card'+-1).classList.add('full-card');
+//         document.getElementById('icon-ball'+-1).classList.add('ball-rotation');
+        
+//         showNavigation();
+//         showDetails(selectetPokemon);
 
-function newFullCard(nextPokemon) {
+// }
+
+
+// function showNavigation(){
+//     document.getElementById('card-1').innerHTML += /*html*/ `
+
+//     `;
+// }
+
+
+function next() {
+    closeCard();
+    showFullCard(selectetPokemon+1)
+    console.log('next >',selectetPokemon+1)
+}
+
+
+function newFullCard(operator) {
     closeCard()
-    showFullCard(nextPokemon);
+    if( operator == +1) {
+        console.log('+1', operator)
+    }
+    if (operator == -1){
+        console.log('-1', operator)
+    }
+
+
+
+    //showFullCard(nextPokemon);
 
 }
 
 
 function closeCard() {
-    document.getElementById('overlay').innerHTML = '';
+    //document.getElementById('overlay').innerHTML = '';
     document.getElementById('overlay').classList.add('d-none');
+    //document.getElementById('card-1').classList.remove(validClass);
+    //validClass = '';
 }
 
 
